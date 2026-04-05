290e11e1f72ebbe76dcb38f316d6d34fe6e8643e
DhimazRafif <hanafidhimaz@gmail.com>
2026-04-05 19:49:47 +0700

    menambahkan class DataMahasiswa_103082430003

--- modul7_kelompok_6/Program.cs
+++ modul7_kelompok_6/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok_6;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_103082430003 mahasiswa = new DataMahasiswa_103082430003();
+        mahasiswa.ReadJSON();
+    }
+}
